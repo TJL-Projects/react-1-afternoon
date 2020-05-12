0c057f77ba1754b7001f78bb409913da77fff8df
tjlemperle <tjlemperle@gmail.com>
2020-05-12 16:58:01 -0600

    react day-1

---
+++ src/components/Topics/FilterObject.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class FilterObject extends Component {
+    render(){
+        return(
+            <p>Filter</p>
+        )
+    }
+
+}
+
+export default FilterObject
